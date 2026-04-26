4e7f6e500c4da98a668baaea877fa991840715a3
taitran5420 <taitran5420@gmail.com>
2026-04-26 19:35:28 +0700

    feat: initial spring boot project

---
+++ src/main/java/com/seap/smartfinancetracker/SmartfinancetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.seap.smartfinancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartfinancetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartfinancetrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seap/smartfinancetracker/SmartfinancetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seap.smartfinancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartfinancetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
