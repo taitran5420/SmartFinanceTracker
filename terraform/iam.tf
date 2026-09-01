a87d3baefe883d8c9efd34844003dd0b27df5bd0
taitran5420 <taitran5420@gmail.com>
2026-09-01 23:39:46 +0700

    feat(terraform): configure the IAM

---
+++ terraform/iam.tf
@@ -0,0 +1,75 @@
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = ["ab9d0263244dd0326eb67015705a667e79cfe998"]
+}
+
+resource "aws_iam_role" "github_actions_deploy" {
+  name = "github-actions-deploy-role"
+
+  assume_role_policy = <<EOF
+   {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Effect": "Allow",
+              "Principal": {
+                  "Federated": "arn:aws:iam::310697202929:oidc-provider/token.actions.githubusercontent.com"
+              },
+              "Action": "sts:AssumeRoleWithWebIdentity",
+              "Condition": {
+                  "StringEquals": {
+                      "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+                  },
+                  "StringLike": {
+                      "token.actions.githubusercontent.com:sub": [
+                          "repo:taitran5420/SmartFinanceTracker:ref:refs/heads/main",
+                          "repo:taitran5420/SmartFinanceTrackerFrontEnd:ref:refs/heads/main"
+                      ]
+                  }
+              }
+          }
+      ]
+  }
+    EOF
+}
+
+resource "aws_iam_role_policy" "github_actions_deploy" {
+  name = "ecr-push-pull"
+  role = aws_iam_role.github_actions_deploy.id
+
+  policy = <<EOF
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Sid": "ECRAuth",
+              "Effect": "Allow",
+              "Action": "ecr:GetAuthorizationToken",
+              "Resource": "*"
+          },
+          {
+              "Sid": "ECRPushPull",
+              "Effect": "Allow",
+              "Action": [
+                  "ecr:BatchCheckLayerAvailability",
+                  "ecr:GetDownloadUrlForLayer",
+                  "ecr:BatchGetImage",
+                  "ecr:PutImage",
+                  "ecr:InitiateLayerUpload",
+                  "ecr:UploadLayerPart",
+                  "ecr:CompleteLayerUpload"
+              ],
+              "Resource": [
+                  "arn:aws:ecr:ap-southeast-1:310697202929:repository/smartfinancetracker-backend",
+                  "arn:aws:ecr:ap-southeast-1:310697202929:repository/smartfinancetracker-frontend"
+              ]
+          }
+      ]
+  }
+  EOF
+}
